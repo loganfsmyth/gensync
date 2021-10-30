d4e09f26e449a0e9628e90d6ca6ab86b4b4affc9
codejedi365 <codejedi365@users.noreply.github.com>
2021-10-30 13:53:13 -0500

    fix-test: modern `require()` to get `promisify()`

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const promisify = require("util.promisify");
+const promisify = require("util").promisify;
 const gensync = require("../");
 
 const TEST_ERROR = new Error("TEST_ERROR");
